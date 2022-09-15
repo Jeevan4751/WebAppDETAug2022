85ba4521bde6a04d9c2c99390318977790c38e1b
githubjeevan <jeevan1999thanu@gmail.com>
2022-09-15 15:25:59 +0530

    WEBAPI Unit Test

---
+++ Library.API/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Library.API.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
